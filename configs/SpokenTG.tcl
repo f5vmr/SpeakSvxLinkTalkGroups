8252bae318fe9746d70f550189acb468f2e32cd3
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-02-11 20:28:06 +0000

    update

--- configs/SpokenTG.tcl
+++ configs/SpokenTG.tcl
@@ -1,13 +1,20 @@
-# --- Spoken TalkGroup support ----------------------------
+###############################################################################
+# Spoken TalkGroups override for ReflectorV2LogicType
+# Should be sourced after LogicBase.tcl in the reflector namespace
+###############################################################################
 
+# Preserve the original say_talkgroup if it exists
+if {[info procs say_talkgroup] ne ""} {
+    rename say_talkgroup say_talkgroup_default
+}
+
+# Table of custom TG → WAV path
 variable spokenTgTable
 array set spokenTgTable {}
 
 proc loadSpokenTalkGroups {} {
     variable spokenTgTable
-
     set cfg [Config::new "TalkGroups.conf"]
-
     if {[$cfg sectionExists "SpokenTalkGroups"]} {
         foreach tg [$cfg listKeys "SpokenTalkGroups"] {
             set spokenTgTable($tg) [$cfg get "SpokenTalkGroups" $tg]
@@ -15,27 +22,21 @@ proc loadSpokenTalkGroups {} {
     }
 }
 
-# Load at logic init
+# Load the table immediately
 loadSpokenTalkGroups
 
-# Preserve original say_talkgroup if present
-if {[info procs say_talkgroup] ne ""} {
-    rename say_talkgroup say_talkgroup_default
-}
-
+# Override say_talkgroup
 proc say_talkgroup {tg} {
     variable spokenTgTable
-
     if {[info exists spokenTgTable($tg)]} {
         playMsg $spokenTgTable($tg) "" 0
         return
     }
 
+    # fallback to original
     if {[info procs say_talkgroup_default] ne ""} {
         say_talkgroup_default $tg
     } else {
         spellNumber $tg
     }
 }
-
-# --- End Spoken TalkGroup support -------------------------
--- install.sh
+++ install.sh
@@ -1,45 +1,12 @@
 #!/bin/bash
 #here we install the talkgroup project first
-sudo cp /usr/share/svxlink/events.d/LogicBase.tcl /usr/share/svxlink/events.d/LogicBase.tcl.orig
-sudo sed -i '/# End of namespace Logic/i \
-# --- Spoken TalkGroup support ----------------------------\n\
-variable spokenTgTable\n\
-array set spokenTgTable {}\n\
-\n\
-proc loadSpokenTalkGroups {} {\n\
-    variable spokenTgTable\n\
-    set cfg [Config::new "TalkGroups.conf"]\n\
-    if {[$cfg sectionExists "SpokenTalkGroups"]} {\n\
-        foreach tg [$cfg listKeys "SpokenTalkGroups"] {\n\
-            set spokenTgTable($tg) [$cfg get "SpokenTalkGroups" $tg]\n\
-        }\n\
-    }\n\
-}\n\
-\n\
-loadSpokenTalkGroups\n\
-\n\
-if {[info procs say_talkgroup] ne ""} {\n\
-    rename say_talkgroup say_talkgroup_default\n\
-}\n\
-\n\
-proc say_talkgroup {tg} {\n\
-    variable spokenTgTable\n\
-    if {[info exists spokenTgTable($tg)]} {\n\
-        playMsg $spokenTgTable($tg) "" 0\n\
-        return\n\
-    }\n\
-    if {[info procs say_talkgroup_default] ne ""} {\n\
-        say_talkgroup_default $tg\n\
-    } else {\n\
-        spellNumber $tg\n\
-    }\n\
-}\n\
-# --- End Spoken TalkGroup support -------------------------\
-' /usr/share/svxlink/events.d/LogicBase.tcl
+sudo cp /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl.orig
+grep -q 'SpokenTG.tcl' /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl || \
+sudo cp SpeakSvxLinkTalkGroups/SpokenTG.tcl /usr/share/svxlink/events.d/ && 
 # that's the logic that adds our code to the LogicBase.tcl
 # now we move the talkgroup wav files to a good location
 sudo mkdir -p /usr/share/svxlink/sounds/en_GB/Custom
-sudo cp -r SpeakSvxLinkTalkGroups/customTG/* /usr/share/svxlink/sounds/en_GB/Custom/
-sudo cp -r SpeakSvxLinkTalkGroups/configs/TalkGroups.conf /etc/svxlink/svxlink.d/
+sudo cp -rup SpeakSvxLinkTalkGroups/customTG/* /usr/share/svxlink/sounds/en_GB/Custom/
+sudo cp -rup SpeakSvxLinkTalkGroups/configs/TalkGroups.conf /etc/svxlink/svxlink.d/
 sudo systemctl restart svxlink
 
