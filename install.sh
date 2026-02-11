bcccaf42a00bcaf2f338fd5beadb6e5beb834221
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-02-11 19:44:48 +0000

    update

--- install.sh
+++ install.sh
@@ -34,11 +34,11 @@ proc say_talkgroup {tg} {\n\
     }\n\
 }\n\
 # --- End Spoken TalkGroup support -------------------------\
-' LogicBase.tcl
+' /usr/share/svxlink/events.d/LogicBase.tcl
 # that's the logic that adds our code to the LogicBase.tcl
 # now we move the talkgroup wav files to a good location
 sudo mkdir -p /usr/share/svxlink/en_GB/sounds/Custom
-sudo cp -r talkgroup/sounds/* /usr/share/svxlink/en_GB/sounds/Custom/
-sudo cp -r talkgroup/TalkGroups.conf /etc/svxlink/svxlink.d/
+sudo cp -r SpeakSvxLinkTalkGroups/talkgroup/sounds/* /usr/share/svxlink/en_GB/sounds/Custom/
+sudo cp -r SpeakSvxLinkTalkGroups/talkgroup/TalkGroups.conf /etc/svxlink/svxlink.d/
 sudo systemctl restart svxlink
 
