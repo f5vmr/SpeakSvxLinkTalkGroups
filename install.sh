e0869bfa9266f6d87e94a0f42c157a649e9479d6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-02-11 20:30:29 +0000

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #here we install the talkgroup project first
 sudo cp /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl.orig
 grep -q 'SpokenTG.tcl' /usr/share/svxlink/events.d/ReflectorV2LogicType.tcl || \
-sudo cp SpeakSvxLinkTalkGroups/SpokenTG.tcl /usr/share/svxlink/events.d/ && 
+sudo cp SpeakSvxLinkTalkGroups/configs/SpokenTG.tcl /usr/share/svxlink/events.d/ && 
 # that's the logic that adds our code to the LogicBase.tcl
 # now we move the talkgroup wav files to a good location
 sudo mkdir -p /usr/share/svxlink/sounds/en_GB/Custom
